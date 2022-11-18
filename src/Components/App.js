a376911804b58db584b2ecc2cc6c72f7bbf1a5e5
manoj barange <manojbrnj111@gmail.com>
2022-11-18 23:26:40 +0530

    react intro

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import List from './List';
+import Main from './Main';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
-
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <List />
+        <Main />
+      </div>
+    );
   }
 }
-
 export default App;
+//render component is dom
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+// access metho by var
 import ReactDOM from 'react-dom';
+//reactdom render our component
 import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//components are objects.
