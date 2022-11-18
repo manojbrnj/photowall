2945d16297351dd8a55f05a02f2cbda69fb9d07d
manoj barange <manojbrnj111@gmail.com>
2022-11-18 14:14:31 +0530

    first commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,14 @@
-import Main from './Main'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import * as actions from '../redux/actions'
-import {withRouter} from 'react-router'
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function mapStateToProps(state){
-    return {
-        posts: state.posts,
-        comments: state.comments,
-        user: state.user
-    }
+    this.state = {};
+  }
+
+  render() {
+    return <div>App</div>;
+  }
 }
 
-function mapDispatchToProps(dispatch){
-    return bindActionCreators(actions, dispatch)
-}
-
-const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main))
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import {BrowserRouter} from 'react-router-dom'
-import {createStore, applyMiddleware, compose} from 'redux'
-import rootReducer from './redux/reducer'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './css/Styles.css'
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
-
-ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
